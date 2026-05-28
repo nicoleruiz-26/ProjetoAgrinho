2c9995d773ecc2a6276dcb1af5c11062d71bb192
nicoleruiz-26 <nicole.ruiz@escola.pr.gov.br>
2026-05-28 08:18:08 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    document.getElementById("mensagem").innerHTML =
+        "As nascentes são protegidas por lei e são essenciais para garantir água limpa para as futuras gerações!";
+}
